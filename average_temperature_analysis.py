27dbaec145e7316a5a888f8ac0e673e7178a9349
AdamKozakowski <adam.kozakowski1@gmail.com>
2024-03-06 14:37:12 +0100

    Initial commit

---
+++ average_temperature_analysis.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+#Get the average temperature data for St. Louis, Missouri from NOOA website. Import dataset
+monthly_average_temperature_df= pd.read_csv('monthly_average_temperature_data.csv')
+
+
+
